8a23675e0f646110d69e52a7cabb40fea6e67b32
AdamApe <sghari8@gmail.com>
2023-03-14 11:22:07 +0100

    Added data_path variable

--- initial_scripts/skull_extraction.sh
+++ initial_scripts/skull_extraction.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/bash
 
-fast -o sub-Ebdo_ses-01_rec-mean_petra_roi5_bias.nii.gz -l 3 -b -B sub-Ebdo_ses-01_rec-mean_petra_roi5.nii.gz
+data_path="/envau/work/nit/users/sghari.a/Analysis/10_03_23/"
+k_dil_ero=5
+thr_head=125
+thr_skull=120
 
-fslmaths sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore.nii.gz -thr 125 -bin sub-Ebdo_ses-01_restore_head_125.nii.gz
 
-fslmaths sub-Ebdo_ses-01_restore_head_125.nii.gz -kernel boxv 5 -dilD sub-Ebdo_ses-01_restore_head_125_dilD5.nii.gz
+fast -o ${data_path}/sub-Ebdo_ses-01_rec-mean_petra_roi5_bias.nii.gz -l 3 -b -B ${data_path}/sub-Ebdo_ses-01_rec-mean_petra_roi5.nii.gz
 
-fslmaths sub-Ebdo_ses-01_restore_head_125_dilD5.nii.gz -kernel boxv 5 -ero sub-Ebdo_ses-01_restore_head_125_dilD5_ero5.nii.gz
+fslmaths ${data_path}/sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore.nii.gz -thr 125 -bin ${data_path}/sub-Ebdo_ses-01_restore_head_125.nii.gz
+
+fslmaths ${data_path}/sub-Ebdo_ses-01_restore_head_125.nii.gz -kernel boxv 5 -dilD ${data_path}/sub-Ebdo_ses-01_restore_head_125_dilD5.nii.gz
+
+fslmaths ${data_path}/sub-Ebdo_ses-01_restore_head_125_dilD5.nii.gz -kernel boxv 5 -ero ${data_path}/sub-Ebdo_ses-01_restore_head_125_dilD5_ero5.nii.gz
 
 #fslmaths sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore.nii.gz -recip -log sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore_recip_log.nii.gz
 
 #fslmaths sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore_recip_log.nii.gz -mas sub-Ebdo_ses-01_restore_head_125_dilD5_ero5.nii.gz sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore_recip_log_masked.nii.gz
 
-fslmaths sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore.nii.gz -mas sub-Ebdo_ses-01_restore_head_125_dilD5_ero5.nii.gz sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore_masked.nii.gz
+fslmaths ${data_path}/sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore.nii.gz -mas ${data_path}/${data_path}/sub-Ebdo_ses-01_restore_head_125_dilD5_ero5.nii.gz sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore_masked.nii.gz
 
-fslmaths sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore_masked.nii.gz -uthr 120 sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore_masked_uthr120.nii.gz
+fslmaths ${data_path}/sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore_masked.nii.gz -uthr 120 ${data_path}/sub-Ebdo_ses-01_rec-mean_petra_roi5_bias_restore_masked_uthr120.nii.gz
 
 
 
